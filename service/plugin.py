add5fbc89e82907baf129cccbf23fde88687d4eb
Sirirat284 <dodo0989263290.gmail.com>
2022-11-12 16:36:59 +0700

    updtae

--- service/plugin.py
+++ service/plugin.py
@@ -5,8 +5,8 @@ def ConnectorMysql():
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="",
-        database="AI-Chatbot",
+        passwd="1234",
+        database="duckbot",
         auth_plugin='mysql_native_password'
     )
     print("Connection Success !")
