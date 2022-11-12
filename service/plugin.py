44dfc52afd3776aaacef0814702f39c167f597a8
Jialing Wang <annsanst62@gmail.com>
2022-11-12 17:51:03 +0700

    upadate answer

--- service/plugin.py
+++ service/plugin.py
@@ -1,20 +1,9 @@
 import json
-import mysql.connector
-
-def ConnectorMysql():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="AI-Chatbot",
-        auth_plugin='mysql_native_password'
-    )
-    print("Connection Success !")
-    return mydb
+from database.config import Connector as db
     
 #def findsubjectbyjob() *
 def getSubbyJOb(job_name):
-    mydb = ConnectorMysql()
+    mydb = db.ConnectorMysql()
     mycursor = mydb.cursor()
     sql = "SELECT groupjob_id FROM Job WHERE job_name = '{}';".format(job_name)
     mycursor.execute(sql)
@@ -28,7 +17,7 @@ def getSubbyJOb(job_name):
 
 #def getsub by job and period
 def getSubbyJobnPeriod(groupjob_id,period):
-    mydb = ConnectorMysql()
+    mydb = db.ConnectorMysql()
     mycursor = mydb.cursor()
     sql = "SELECT subject_id,subject_name ,time FROM Subject WHERE groupjob_id = %s AND period = %s "
     val = (groupjob_id, period)
@@ -44,7 +33,7 @@ def getSubbyJobnPeriod(groupjob_id,period):
     
 #def findsubjectbytiming() *
 def getSubbyTime(groupjob_id,time,day):
-    mydb = ConnectorMysql()
+    mydb = db.ConnectorMysql()
     mycursor = mydb.cursor()
     sql = "SELECT subject_id,subject_name ,time FROM Subject WHERE groupjob_id = %s AND period = %s AND day = %s"
     val = (groupjob_id,time,day)
@@ -60,7 +49,7 @@ def getSubbyTime(groupjob_id,time,day):
 
 #get data by groupjob only *
 def getSubbyGroupjob(groupjob_id):
-    mydb = ConnectorMysql()
+    mydb = db.ConnectorMysql()
     mycursor = mydb.cursor()
     sql = "SELECT subject_id,subject_name ,time,day FROM Subject WHERE groupjob_id = '{}';".format(groupjob_id)
     mycursor.execute(sql)
@@ -73,7 +62,7 @@ def getSubbyGroupjob(groupjob_id):
 
 #use for get subjectby time only *
 def getSubbyTimeNG(time, day):
-    mydb = ConnectorMysql()
+    mydb = db.ConnectorMysql()
     mycursor = mydb.cursor()
     sql = "SELECT subject_id,subject_name ,time FROM Subject WHERE period = %s AND day = %s"
     val = (time, day)
@@ -88,7 +77,7 @@ def getSubbyTimeNG(time, day):
 
 #get all subject in that semester
 def getAllSub():
-    mydb = ConnectorMysql()
+    mydb = db.ConnectorMysql()
     mycursor = mydb.cursor()
     sql = "SELECT subject_id,subject_name,time,day FROM Subject "
     mycursor.execute(sql)
